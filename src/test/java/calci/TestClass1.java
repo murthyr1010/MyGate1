d0cd60d57c8a81bd9d96bbe345710979e4670ee6
murthyr1010 <murthyr1010@gmail.com>
2021-03-20 11:47:40 +0530

    Update TestClass1.java

--- src/test/java/calci/TestClass1.java
+++ src/test/java/calci/TestClass1.java
@@ -16,5 +16,16 @@ public class TestClass1 {
 	{
 		Assert.assertEquals(5+8, 13);
 	}
+	
+	@Test
+	public void test13()
+	{
+		Assert.assertEquals(56+8, 13);
+	}
+	@Test
+	public void test14()
+	{
+		Assert.assertEquals(6+8, 13);
+	}
 
 }
